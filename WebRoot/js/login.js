61b289fed64d470c09e60d18e545416e6df3dfa2
liuyang <liuyang@windows10.microdone.cn>
2017-07-03 11:41:44 +0800

    增加了验证数据完整性的负逻辑

--- WebRoot/js/login.js
+++ WebRoot/js/login.js
@@ -97,11 +97,11 @@ var Login = (function(){
 		//data即从服务器中拿到的公钥
 		//如果确实拿到了公钥,则将其set进encrypt成员中,以便之后的对称加密
 		if(data.publicKey!=null&&data.extra!=null){
-			//对数据完整性做检查
-			if(encrypt.encrypt_MD5(data.publicKey)==data.extra){
 	        	encrypt.setPublicKey(data.publicKey);
-			}
+
 		}
+		//对数据完整性做检查
+		if(encrypt.encrypt_MD5(data.publicKey)==data.extra){
 		//使用公钥对对称秘钥进行非对称加密加密
 		var encryptKey=encrypt.encrypt_RSA(Common.key.value);
 		//使用3DES对管理员账号进行加密
@@ -124,6 +124,10 @@ var Login = (function(){
 			"extra":encryptCheckEncode
 		};
 		Common.ajax("admin/shakeHand", "post", data, login_3);
+		}else{
+			$("#errInfo").html("登陆失败，请刷新后重试");
+			$("#error").show();
+		}
 	};
 	
 	//客户端登录握手第三步
