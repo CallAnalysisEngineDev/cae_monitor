02032b24583eca9ba1ee871258acc1016881a991
xiongjunlang <xiongjunlang@agree.com.cn>
2017-07-04 11:53:02 +0800

    补充文件系统的实体类内容

--- src/org/cae/monitor/entity/FileSystem.java
+++ src/org/cae/monitor/entity/FileSystem.java
@@ -2,4 +2,12 @@ package org.cae.monitor.entity;
 
 public class FileSystem {
 
+	private String fileSystemName;
+	private Integer fileSystemAvail;
+	public String getFileSystemName() {
+		return fileSystemName;
+	}
+	public Integer getFileSystemAvail() {
+		return fileSystemAvail;
+	}
 }
