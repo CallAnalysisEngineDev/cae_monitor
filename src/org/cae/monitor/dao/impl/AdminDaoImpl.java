28a4dff77044254efa6b31b195398a2ca6a17b91
xiongjunlang <xiongjunlang@agree.com.cn>
2017-11-14 17:20:36 +0800

    使用lambda改良代码,并将项目升级到jdk8

--- src/org/cae/monitor/dao/impl/AdminDaoImpl.java
+++ src/org/cae/monitor/dao/impl/AdminDaoImpl.java
@@ -1,7 +1,5 @@
 package org.cae.monitor.dao.impl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -14,7 +12,6 @@ import org.cae.monitor.dao.IAdminDao;
 import org.cae.monitor.entity.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository("adminDao")
@@ -31,29 +28,16 @@ public class AdminDaoImpl implements IAdminDao {
 
 	@Override
 	public DaoResult getAdminInfoDao(Admin admin) {
-		String sql = "SELECT 1 " + "FROM admin "
-				+ "WHERE admin_useraccount = ?";
-		List<String> list = template.queryForList(sql,
-				new Object[] { admin.getAdminUseraccount() }, String.class);
+		String sql = "SELECT 1 " + "FROM admin " + "WHERE admin_useraccount = ?";
+		List<String> list = template.queryForList(sql, new Object[] { admin.getAdminUseraccount() }, String.class);
 		if (list.size() == 0) {
 			return new DaoResult(false, "管理员账号不存在");
 		}
-		sql = "SELECT admin_id " + "FROM admin "
-				+ "WHERE admin_useraccount = ? " + "AND admin_password = ?";
+		sql = "SELECT admin_id " + "FROM admin " + "WHERE admin_useraccount = ? " + "AND admin_password = ?";
 		// 密码要进行md5处理后才和数据库的对比
-		List<Admin> theResult = template.query(
-				sql,
-				new Object[] { admin.getAdminUseraccount(),
-						md5(admin.getAdminPassword()) },
-				new RowMapper<Admin>() {
-
-					@Override
-					public Admin mapRow(ResultSet rs, int row)
-							throws SQLException {
-						return Generator.admin(rs.getInt("admin_id"));
-					}
-
-				});
+		List<Admin> theResult = template.query(sql,
+				new Object[] { admin.getAdminUseraccount(), md5(admin.getAdminPassword()) },
+				(rs, row) -> Generator.admin(rs.getInt("admin_id")));
 		if (isNotNull(theResult)) {
 			return new DaoResult(true, theResult.get(0));
 		} else {
--- src/org/cae/monitor/service/impl/AdminServiceImpl.java
+++ src/org/cae/monitor/service/impl/AdminServiceImpl.java
@@ -35,8 +35,7 @@ public class AdminServiceImpl implements IAdminService {
 		ServiceResult theResult = null;
 		if (shakeHand.getSummary().equals(Util.md5(shakeHand.getMessage()))) {
 			// 先把客户端传来的json字符串转换为java的map
-			Map<String, Object> map = toObject(shakeHand.getMessage(),
-					Map.class);
+			Map<String, Object> map = toObject(shakeHand.getMessage(), Map.class);
 			// 获取经过加密的对称秘钥并使用rsa的秘钥解密,得到真实的对称秘钥
 			String key = rsa.decrypt((String) map.get("k"));
 			// 实例化一个3DES算法的实例,并把对称秘钥通过构造函数注入进去
@@ -46,9 +45,8 @@ public class AdminServiceImpl implements IAdminService {
 			// 使用3des对密码进行解密,得到真实的密码
 			String password = desede.decrypt((String) map.get("p"));
 			// 调用dao层方法,判断是否登录成功
-			DaoResult daoResult = adminDao.getAdminInfoDao(new Admin.Builder()
-					.adminUseraccount(userAccount).adminPassword(password)
-					.build());
+			DaoResult daoResult = adminDao
+					.getAdminInfoDao(new Admin.Builder().adminUseraccount(userAccount).adminPassword(password).build());
 			// 如果登录成功,则把上面已经实例化出来的3des对象放入keys中,这样之后的通信都可以根据管理员id来获取对称秘钥了
 			if (daoResult.isSuccessed()) {
 				Admin admin = (Admin) daoResult.getResult();
